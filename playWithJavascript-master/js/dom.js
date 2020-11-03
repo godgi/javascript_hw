6057857f73791e74e7ad776ce82b2f8985f99b4f
godgi <godgi871@nate.com>
2020-11-03 22:57:37 +0900

    hw

---
+++ playWithJavascript-master/js/dom.js
@@ -0,0 +1,4 @@
+function hideH3P() {
+    document.querySelector('#detail p').style.visibility='hidden';
+    document.querySelector('#detail h3').style.visibility='hidden';
+    }
---
+++ playWithJavascript-master/js/showBig.js
@@ -0,0 +1,13 @@
+var bigPic = document.querySelector('#cup');
+var smallPics = document.querySelectorAll('.small');
+
+
+for(var i=0; i<smallPics.length; i++) {
+    smallPics[i].addEventListener('click', changePic);
+}
+
+
+function changePic() {
+    var newPic = this.src;
+    bigPic.setAttribute('src', newPic);
+}
